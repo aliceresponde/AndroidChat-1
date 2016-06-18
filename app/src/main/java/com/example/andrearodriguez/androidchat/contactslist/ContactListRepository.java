bc35a7a56422abc17c15dbaafd0bb55fe4841e2c
Alicia Beltran <aliceresponde@gmail.com>
2016-06-18 00:02:32 -0500

    implementado el  repositoy

--- app/src/main/java/com/example/andrearodriguez/androidchat/contactslist/ContactListRepository.java
+++ app/src/main/java/com/example/andrearodriguez/androidchat/contactslist/ContactListRepository.java
@@ -1,10 +1,15 @@
 package com.example.andrearodriguez.androidchat.contactslist;
 
+import com.example.andrearodriguez.androidchat.domain.FirebaseHelper;
+import com.google.firebase.database.ChildEventListener;
+
 /**
  * Created by alice on 6/16/16.
  */
 
 public interface ContactListRepository {
+
+
 
 
     //session
